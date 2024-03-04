b158fb94fddd690a06ab14de4455327a05605d25
YDongGeun <97018542+YDongGeun@users.noreply.github.com>
2024-03-04 21:02:30 +0900

    초기 버전 세팅
    초기버전 팀원과 맞추기

---
+++ src/main/java/com/github/sc_first_project/ScFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.sc_first_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScFirstProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScFirstProjectApplication.class, args);
+    }
+
+}
