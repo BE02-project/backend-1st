339668256c99796e3fdeb26110702c5c44a42998
Joojinsung <dev.jinsung1017@gmail.com>
2024-03-05 12:47:45 +0900

    encoding 완료~~

--- src/main/java/com/github/sc_first_project/config/EncoderConfig.java
+++ src/main/java/com/github/sc_first_project/config/EncoderConfig.java
@@ -1,4 +1,15 @@
 package com.github.sc_first_project.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class EncoderConfig {
+
+    //순환 참조 문제로 따로 빼놨어요
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
